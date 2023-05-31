46c09634cc7857a7840fdec695ba72bb7f200b47
richardJET <richardtillo@gmail.com>
2023-05-30 22:44:24 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import PickTable from './components/PickTable';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App bg-white">
       <Header />
       <PickTable />
+      <Footer />
     </div>
   );
 }
